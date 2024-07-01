fa1cea0fb1c4c86c26b8a3edc05deed03d12b56d
Adrián Scotto <adrian.scotto@socialpath.com.ar>
2024-07-01 17:39:57 -0300

    FIX: Ahora sí genera las configs necesarias para que sea reconocida como PWA

--- svelte.config.js
+++ svelte.config.js
@@ -1,40 +1,8 @@
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/sag_poc/', // GitHub repository name
   build: {
     assetsDir: '.', // Use relative assets directory
   },  
-  plugins: [
-    svelte({
-      preprocess: vitePreprocess()
-    }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'sag_poc',
-        short_name: 'sag_poc',
-        description: 'My Svelte PWA Proof of Concept',
-        start_url: './',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#ff3e00',
-        icons: [
-          {
-            src: './icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: './icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
 });
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: vitePreprocess()
+    }),   
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'sag_poc',
+        short_name: 'sag_poc',
+        description: 'My Svelte PWA Proof of Concept',
+        start_url: '/sag_poc/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ff3e00',
+        icons: [
+          {
+            src: './icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
